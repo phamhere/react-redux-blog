cd73087b37c79f9ebc35c7cc552103abb4625f16
Peter Pham <phamhere@gmail.com>
2018-12-23 16:05:42 -0700

    added postsreducer for fetchposts action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,13 +3,17 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
 const mapStateToProps = state => {
-  return state;
+  return { posts: state.posts };
 };
 
 export default connect(
